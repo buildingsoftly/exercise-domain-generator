7b3c5090e8773aef9af7aa830279184782a3f697
buildingsoftly <a.c.mcmurry@proton.me>
2025-02-24 19:03:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './src',
+  build: {
+    outDir: '../dist',
+  },
+  server: {
+    port: 3000,
+  },
+});
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
